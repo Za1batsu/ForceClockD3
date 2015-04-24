14dafd56bca827f1fe766176ffed795143d57ef0
Kyle Hilton <kylehilton92@gmail.com>
2015-04-24 08:46:51 -0700

    refactor and readd status check

--- forceClock.js
+++ forceClock.js
@@ -163,4 +163,31 @@ var init = function(date) {
   updateDig();
 }(new Date()); 
 
+//-----------------------------
+//This handles the FPS from stats.js
+// var stats = new Stats();
+// stats.setMode(0); // 0: fps, 1: ms
+
+// // align top-left
+// stats.domElement.style.position = 'absolute';
+// stats.domElement.style.left = '0px';
+// stats.domElement.style.top = '0px';
+
+// document.body.appendChild(stats.domElement);
+
+// var updateStats = function() {
+
+//   stats.begin();
+
+//   // monitored code goes here
+
+//   stats.end();
+
+//   requestAnimationFrame(updateStats);
+
+// };
+//------------------------------
+
+ // requestAnimationFrame(updateStats); //Call updateStates for every frame
+
 d3.timer(update, 500);
